54bba3f3785bdfc7f87c2f8d541ab3fe7f306f34
Adham Nour <adhamnourelwaffaa@gmail.com>
2021-09-23 18:31:39 +0200

    Add project files.

---
+++ PhoneApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PhoneApplication.Services;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<Contact> contacts;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            contacts = ContactServices.GetAll();
+        }
+    }
+}
---
+++ PhoneApplication/Services/ContactServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Services
+{
+    public static class ContactServices
+    {
+        static List<Contact> contacts { get; }
+        static ContactServices()
+        {
+            contacts = new List<Contact>
+            {
+                new Contact
+                {
+                    Name = "Adham",
+                    PhoneNumber="01001516725"
+                }, 
+                new Contact
+                {
+                    Name = "Khaled",
+                    PhoneNumber="01552423"
+                }, 
+                new Contact
+                {
+                    Name = "Hussing",
+                    PhoneNumber="01442255330"
+                },
+
+            };
+        }
+        public static List<Contact> GetAll() => contacts;
+
+    }
+}
